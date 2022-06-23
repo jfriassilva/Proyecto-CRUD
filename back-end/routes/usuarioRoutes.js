45b3589139877dcc343db7045ed34ca445c9a930
Jeronimo Frias Silva <jfriassi@emeal.nttdata.com>
2022-06-23 14:03:14 -0300

    hasheando contraseña y usuario

--- back-end/routes/usuarioRoutes.js
+++ back-end/routes/usuarioRoutes.js
@@ -4,5 +4,6 @@ import { registrar } from '../controllers/usuarioController.js';
 
 // Creacion, Registro y Confirmacion de usuarios
 router.post( "/", registrar);
+router.post("/login", autenticar)
 
 export default router;
