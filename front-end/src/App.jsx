bef28978633deff2cc2a55d136eb3e19e6c99695
Jeronimo Frias Silva <jfriassi@emeal.nttdata.com>
2022-07-01 12:03:14 -0300

    agregando context y hooks

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -13,28 +13,31 @@ import OlvidePassword from './paginas/OlvidePassword'
 import NuevoPassword from './paginas/NuevoPassword'
 import ConfirmarCuenta from './paginas/ConfirmarCuenta'
 
+import {AuthProvider} from './context/AuthProvider'
+
 
 function App() {
 
   
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<AuthLayout/>}>
-        <Route index element={<Login/>} />
-        <Route path="registrar" element={<Registrar />} />
-        <Route path="olvide-password" element={<OlvidePassword/>} />
-        <Route path="olvide-password/:token" element={<NuevoPassword/>} />
-        <Route path="confirmar/:id" element={<ConfirmarCuenta/>} />
-        </Route>
-
-        <Route path="/pacientes"  element={<Layout/>}>
-          <Route index element={<Inicio />} />
-          <Route path="nuevo" element={<NuevoPaciente />} />
-          <Route path="editar/:id" element={<EditarPaciente />} />
-          <Route path=":id" element={<VerPaciente />} />
-        </Route>
-    </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<AuthLayout/>}>
+            <Route index element={<Login/>} />
+            <Route path="registrar" element={<Registrar />} />
+            <Route path="olvide-password" element={<OlvidePassword/>} />
+            <Route path="olvide-password/:token" element={<NuevoPassword/>} />
+            <Route path="confirmar/:id" element={<ConfirmarCuenta/>} />
+          </Route>
+          <Route path="/pacientes"  element={<Layout/>}>
+            <Route index element={<Inicio />} />
+            <Route path="nuevo" element={<NuevoPaciente />} />
+            <Route path="editar/:id" element={<EditarPaciente />} />
+            <Route path=":id" element={<VerPaciente />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
