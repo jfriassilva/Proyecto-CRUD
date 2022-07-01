079750d35675c6e914dba19f7a58d23f1708066b
Jeronimo Frias Silva <jfriassi@emeal.nttdata.com>
2022-07-01 13:00:14 -0300

    continuando con el hook

--- front-end/src/context/AuthProvider.jsx
+++ front-end/src/context/AuthProvider.jsx
@@ -5,10 +5,12 @@ const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
 
+    const [auth, setAuth] = useState({})
+
     return (
         <AuthContext.Provider
             value={{
-
+                setAuth
             }}
         >
             {children}
