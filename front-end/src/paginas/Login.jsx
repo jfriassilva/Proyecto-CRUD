f3e0cae08a5e9a02fe379b725dc52f598f9a33bd
Jeronimo Frias Silva <jfriassi@emeal.nttdata.com>
2022-07-01 09:29:12 -0300

    agregando autenticar al login

--- front-end/src/paginas/Login.jsx
+++ front-end/src/paginas/Login.jsx
@@ -1,11 +1,51 @@
-import {Link} from "react-router-dom" 
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom" 
+import Alerta from "../componentes/Alerta"
+import usuarioAxios from "../config/usuarioAxios"
+
 const Login = () => {
+    const [email, setEmail] = useState(' ')
+    const [password, setPassword] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        if([email, password].includes(' ')) {
+            setAlerta({
+                msg: 'Todos los campos son obligatorios',
+                error: true
+            });
+            return
+        }
+
+        try {
+            const { data } = await usuarioAxios.post('/usuarios/login', {email, password})
+            setAlerta({})
+            localStorage.setItem('token', data.token)
+        } catch (error) {
+            setAlerta ({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+
+    }
+
+    const { msg } = alerta
+
+
   return (
     <>
         <h1 className="text-sky-600 font-black text-6xl capitalize text-center">Inicia sesión y administra tus 
         <span className="text-slate-700"> pacientes</span></h1>
 
-        <form className="my-10 bg-white shadow rounded-lg p-10">
+        {msg && <Alerta alerta={alerta} />}
+
+        <form 
+            className="my-10 bg-white shadow rounded-lg p-10"
+            onSubmit={handleSubmit}
+        >
             <div className="my-5">
                 <label 
                     className="uppercase text-gray-600 block text-xl font-bold"
@@ -16,6 +56,8 @@ const Login = () => {
                     type="email" 
                     placeholder="Email de Registro"
                     className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                    value={email}
+                    onChange= { e => setEmail(e.target.value)}
                 />
             </div>
             <div className="my-5">
@@ -28,6 +70,8 @@ const Login = () => {
                     type="password" 
                     placeholder="Contraseña de Registro"
                     className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                    value={password}
+                    onChange= { e => setPassword(e.target.value)}
                 />
             </div>
             <input type="submit" value="Iniciar Sesión" className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"/>
